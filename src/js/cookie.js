29288c0f8e123e9dbf782f5ff26661687e68f842
gleniolobo <gleniolobo2009@gmail.com>
2023-10-01 23:40:03 -0300

    jsdocs

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,12 +1,22 @@
+// @ts-check
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+/** Class representing a cookie message */
 class CookieMessage{
+    /** Creates a cookie message */
     constructor(){
+        /**
+         * @property {string} message Cookie message
+         */
         this.message = 
             'We use cookies for improving functionality and analytics. <button class="btn cookie__btn">Got it!</button>';
     }
 
+    /** 
+     * @property {Fuction} createCookieMessage Render cookie message 
+     * @returns  {HTMLElement} A div element containing the cookie message
+    */
     createCookieMessage(){
         const divCookie = document.createElement('div');
         divCookie.classList.add('cookie');
@@ -16,10 +26,16 @@ class CookieMessage{
 }
 
 const headerCookie = document.querySelector('.header');
+
+/**
+ * [See CookieMessage Class]{@link CookieMessage}
+ */
 const cookieMessage = new CookieMessage().createCookieMessage();
 
+//@ts-ignore
 headerCookie.append(cookieMessage);
 
+// @ts-ignore
 document.querySelector('.cookie__btn').addEventListener('click', function(e){
         e.preventDefault();
         cookieMessage.remove();
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,4 @@
+// @ts-check
 'use strict';
 
 import 'core-js/stable';
@@ -7,32 +8,54 @@ const singUpNav = document.querySelector('.navbar__btn');
 const singUpRecommendation = document.querySelector('.account__btn');
 const closeModal =  document.querySelector('.modal__close');
 
+/**
+ * Class Representing a Modal
+ */
 class Modal{
+    /**
+     * Create a modal
+     * @param {HTMLElement} modal
+     * @param {HTMLElement} overlay
+     */
     constructor(modal, overlay){
+        /** @property {HtmlElement} modal Modal Html Element */
         this.modal = modal;
+
+        /** @property {HTMLElement} overlay Overlay Html Element */
         this.overlay = overlay;
     }
 
+    /** 
+     * @property {Fuction} singUpButtonClick Control the visibility of the modal 
+     * @returns  {void}
+    */
     singUpButtonClick(){
         this.overlay.classList.toggle('hidden');
         this.modal.classList.toggle('hidden');
     }
 }
 
+/**
+ *[See Modal Class]{@link Modal}
+ */
 const bankistModal = new Modal(
+    //@ts-ignore
                                 document.querySelector('.modal'), 
                                 document.querySelector('.overlay'),
                             );
 
 
+//@ts-ignore
 singUpNav.addEventListener("click", 
                             bankistModal.singUpButtonClick.bind(bankistModal)
                         );
 
+//@ts-ignore
 singUpRecommendation.addEventListener('click', 
                             bankistModal.singUpButtonClick.bind(bankistModal)
                         );
 
+//@ts-ignore
 closeModal.addEventListener('click', 
                             bankistModal.singUpButtonClick.bind(bankistModal)
                         );
